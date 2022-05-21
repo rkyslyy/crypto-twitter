549fecf9a3f7c0fdeea97e844626b50409371b27
Roman Kyslyy <roman.kyslyy@invitae.com>
2022-05-21 22:54:01 +0300

    split fetch

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const axios = require("axios").default;
+const Express = require("express");
+const redis = require("redis");
 
 require("dotenv").config();
 
@@ -15,10 +17,29 @@ const CRYPTOS = [
   "Chainlink",
 ];
 
-const run = async () => {
+const wait = () =>
+  new Promise((res) => {
+    setTimeout(() => {
+      res();
+    }, 30000);
+  });
+
+const client = redis.createClient();
+
+client.connect();
+
+const app = Express();
+
+app.use(Express.json());
+
+app.listen(5000, () => {
+  console.log("Listening on 5000...");
+});
+
+const fetchTweetCounts = async (cryptos) => {
   try {
     const res = await Promise.all(
-      CRYPTOS.map((crypto) =>
+      cryptos.map((crypto) =>
         axios.get(`https://api.twitter.com/2/tweets/counts/recent?query=${crypto}`, {
           headers: { Authorization: `Bearer ${process.env.TWITTER_TOKEN}` },
         })
@@ -36,6 +57,18 @@ const run = async () => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const run = async () => {
+  setInterval(() => {
+    fetchTweetCounts(CRYPTOS.slice(0, 5));
+  }, 60000);
+
+  await wait();
+
+  setInterval(() => {
+    fetchTweetCounts(CRYPTOS.slice(5));
+  }, 60000);
 };
 
 run();
