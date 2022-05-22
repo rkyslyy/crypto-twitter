51472eab70ed8067e55a666c4d3627eb9a3348db
Roman Kyslyy <roman.kyslyy@invitae.com>
2022-05-22 09:12:12 +0300

    x

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: { origin: "http://localhost:3000", methods: ["GET", "POST"] },
-});
+const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
 const redis = require("redis");
 const client = redis.createClient();
 
